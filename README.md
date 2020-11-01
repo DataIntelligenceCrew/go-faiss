371e62ef217c78f4780b74723b0e11cb379a9ef9
Paul Ouellette <pouellet@u.rochester.edu>
2020-11-01 18:07:07 -0500

    Add Index.RemoveIDs

--- README.md
+++ README.md
@@ -4,13 +4,14 @@ Go bindings for [Faiss](https://github.com/facebookresearch/faiss).
 
 ## Install
 
-First you will need to build and install Faiss:
+First you will need to build and install Faiss. The `C-api-cmake` branch at
+https://github.com/DataIntelligenceCrew/faiss/tree/C-api-cmake has some fixes
+for building the C API.
 
 ```sh
-git clone https://github.com/facebookresearch/faiss.git
+git clone https://github.com/DataIntelligenceCrew/faiss.git
 cd faiss
-# Apply patch for building C API with cmake
-curl -L https://github.com/facebookresearch/faiss/pull/1390.patch | git am -3
+git checkout C-api-cmake
 cmake -B build -DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_C_API=ON -DBUILD_SHARED_LIBS=ON
 make -C build
 sudo make -C build install
@@ -18,7 +19,7 @@ sudo make -C build install
 
 Building will produce the dynamic library `faiss_c`.
 You will need to install it in a place where your system will find it (e.g.
-`/usr/lib` or some place in `LD_LIBRARY_PATH` on Linux).
+`/usr/lib` on Linux).
 You can do this with:
 
     sudo cp build/c_api/libfaiss_c.so /usr/lib
--- faiss.go
+++ faiss.go
@@ -8,6 +8,7 @@ package faiss
 #include <faiss/c_api/Index_c.h>
 #include <faiss/c_api/error_c.h>
 #include <faiss/c_api/faiss_c.h>
+#include <faiss/c_api/impl/AuxIndexStructures_c.h>
 */
 import "C"
 
@@ -19,7 +20,6 @@ import (
 // Index is the base structure for an index.
 type Index struct {
 	idx *C.FaissIndex
-	dim int
 }
 
 // IndexFlatIP is an index for maximum inner product search.
@@ -47,7 +47,7 @@ func getLastError() error {
 
 // Add adds vectors to the index.
 func (idx *Index) Add(x [][]float32) error {
-	cx := cVecMatrix(x, idx.dim)
+	cx := cVecMatrix(x, int(C.faiss_Index_d(idx.idx)))
 	defer C.free(unsafe.Pointer(cx))
 
 	c := C.faiss_Index_add(idx.idx, C.idx_t(len(x)), cx)
@@ -63,7 +63,7 @@ func (idx *Index) Add(x [][]float32) error {
 func (idx *Index) Search(x [][]float32, k int) (
 	dist [][]float32, labels [][]int, err error,
 ) {
-	cx := cVecMatrix(x, idx.dim)
+	cx := cVecMatrix(x, int(C.faiss_Index_d(idx.idx)))
 	defer C.free(unsafe.Pointer(cx))
 
 	cl := (*C.idx_t)(C.malloc(C.size_t(len(x)*k) * C.sizeof_idx_t))
@@ -95,6 +95,31 @@ func (idx *Index) Search(x [][]float32, k int) (
 	return dist, labels, nil
 }
 
+// RemoveIDs removes vectors with the given IDs from the index.
+// Returns the number of elements removed and error or nil.
+func (idx *Index) RemoveIDs(ids []int) (int, error) {
+	ci := C.malloc(C.size_t(len(ids)) * C.sizeof_idx_t)
+	defer C.free(ci)
+	gi := (*[1 << 30]C.idx_t)(ci)
+
+	for i, v := range ids {
+		gi[i] = C.idx_t(v)
+	}
+
+	var sel *C.FaissIDSelectorBatch
+	c := C.faiss_IDSelectorBatch_new(&sel, C.size_t(len(ids)), (*C.idx_t)(ci))
+	if c != 0 {
+		return 0, getLastError()
+	}
+
+	var nRemoved C.size_t
+	c = C.faiss_Index_remove_ids(idx.idx, (*C.FaissIDSelector)(sel), &nRemoved)
+	if c != 0 {
+		return 0, getLastError()
+	}
+	return int(nRemoved), nil
+}
+
 // Delete frees the memory used by the index.
 func (idx *Index) Delete() {
 	C.faiss_Index_free(idx.idx)
@@ -102,7 +127,7 @@ func (idx *Index) Delete() {
 
 // NewIndexFlatIP creates a new IndexFlatIP.
 func NewIndexFlatIP(d int) (*IndexFlatIP, error) {
-	index := Index{dim: d}
+	var index Index
 	c := C.faiss_IndexFlatIP_new_with(&index.idx, C.idx_t(d))
 	if c != 0 {
 		return nil, getLastError()
