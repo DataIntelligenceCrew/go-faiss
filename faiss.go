101c7c62481fb792a1824f7be9b1ce56bc546168
Paul Ouellette <pouellet@u.rochester.edu>
2021-04-06 23:48:57 -0400

    Refactor to make Index an interface
    This allows writing functions that take any index.
    SetIndexParameter now works with IndexFlat.
    The old Index struct is renamed to IndexImpl.

--- faiss.go
+++ faiss.go
@@ -45,11 +45,12 @@ func NewParameterSpace() (*ParameterSpace, error) {
 }
 
 // SetIndexParameter sets one of the parameters.
-func (p *ParameterSpace) SetIndexParameter(idx *Index, name string, val float64) error {
+func (p *ParameterSpace) SetIndexParameter(idx Index, name string, val float64) error {
 	cname := C.CString(name)
 	defer C.free(unsafe.Pointer(cname))
 
-	c := C.faiss_ParameterSpace_set_index_parameter(p.ps, idx.idx, cname, C.double(val))
+	c := C.faiss_ParameterSpace_set_index_parameter(
+		p.ps, idx.cPtr(), cname, C.double(val))
 	if c != 0 {
 		return getLastError()
 	}
@@ -152,46 +153,98 @@ const (
 )
 
 // Index is a Faiss index.
-type Index struct {
+//
+// Note that some index implementations do not support all methods.
+// Check the Faiss wiki to see what operations an index supports.
+type Index interface {
+	// D returns the dimension of the indexed vectors.
+	D() int
+
+	// IsTrained returns true if the index has been trained or does not require
+	// training.
+	IsTrained() bool
+
+	// Ntotal returns the number of indexed vectors.
+	Ntotal() int64
+
+	// MetricType returns the metric type of the index.
+	MetricType() int
+
+	// Train trains the index on a representative set of vectors.
+	Train(x []float32) error
+
+	// Add adds vectors to the index.
+	Add(x []float32) error
+
+	// AddWithIDs is like Add, but stores xids instead of sequential IDs.
+	AddWithIDs(x []float32, xids []int64) error
+
+	// Search queries the index with the vectors in x.
+	// Returns the IDs of the k nearest neighbors for each query vector and the
+	// corresponding distances.
+	Search(x []float32, k int64) (distances []float32, labels []int64, err error)
+
+	// RangeSearch queries the index with the vectors in x.
+	// Returns all vectors with distance < radius.
+	RangeSearch(x []float32, radius float32) (*RangeSearchResult, error)
+
+	// Reset removes all vectors from the index.
+	Reset() error
+
+	// RemoveIDs removes the vectors specified by sel from the index.
+	// Returns the number of elements removed and error.
+	RemoveIDs(sel *IDSelector) (int, error)
+
+	// Delete frees the memory used by the index.
+	Delete()
+
+	cPtr() *C.FaissIndex
+}
+
+type faissIndex struct {
 	idx *C.FaissIndex
 }
 
-func indexD(idx *C.FaissIndex) int {
-	return int(C.faiss_Index_d(idx))
+func (idx *faissIndex) cPtr() *C.FaissIndex {
+	return idx.idx
 }
 
-func indexIsTrained(idx *C.FaissIndex) bool {
-	return C.faiss_Index_is_trained(idx) != 0
+func (idx *faissIndex) D() int {
+	return int(C.faiss_Index_d(idx.idx))
 }
 
-func indexNtotal(idx *C.FaissIndex) int64 {
-	return int64(C.faiss_Index_ntotal(idx))
+func (idx *faissIndex) IsTrained() bool {
+	return C.faiss_Index_is_trained(idx.idx) != 0
 }
 
-func indexMetricType(idx *C.FaissIndex) int {
-	return int(C.faiss_Index_metric_type(idx))
+func (idx *faissIndex) Ntotal() int64 {
+	return int64(C.faiss_Index_ntotal(idx.idx))
 }
 
-func indexTrain(idx *C.FaissIndex, x []float32) error {
-	n := len(x) / indexD(idx)
-	if c := C.faiss_Index_train(idx, C.idx_t(n), (*C.float)(&x[0])); c != 0 {
+func (idx *faissIndex) MetricType() int {
+	return int(C.faiss_Index_metric_type(idx.idx))
+}
+
+func (idx *faissIndex) Train(x []float32) error {
+	n := len(x) / idx.D()
+	if c := C.faiss_Index_train(idx.idx, C.idx_t(n), (*C.float)(&x[0])); c != 0 {
 		return getLastError()
 	}
 	return nil
 }
 
-func indexAdd(idx *C.FaissIndex, x []float32) error {
-	n := len(x) / indexD(idx)
-	if c := C.faiss_Index_add(idx, C.idx_t(n), (*C.float)(&x[0])); c != 0 {
+func (idx *faissIndex) Add(x []float32) error {
+	n := len(x) / idx.D()
+	if c := C.faiss_Index_add(idx.idx, C.idx_t(n), (*C.float)(&x[0])); c != 0 {
 		return getLastError()
 	}
 	return nil
 }
 
-func indexAddWithIDs(idx *C.FaissIndex, x []float32, xids []int64) error {
-	n := len(x) / indexD(idx)
+func (idx *faissIndex) AddWithIDs(x []float32, xids []int64) error {
+	n := len(x) / idx.D()
 	if c := C.faiss_Index_add_with_ids(
-		idx,
+		idx.idx,
 		C.idx_t(n),
 		(*C.float)(&x[0]),
 		(*C.idx_t)(&xids[0]),
@@ -201,14 +254,14 @@ func indexAddWithIDs(idx *C.FaissIndex, x []float32, xids []int64) error {
 	return nil
 }
 
-func indexSearch(idx *C.FaissIndex, x []float32, k int64) (
+func (idx *faissIndex) Search(x []float32, k int64) (
 	distances []float32, labels []int64, err error,
 ) {
-	n := len(x) / indexD(idx)
+	n := len(x) / idx.D()
 	distances = make([]float32, int64(n)*k)
 	labels = make([]int64, int64(n)*k)
 	if c := C.faiss_Index_search(
-		idx,
+		idx.idx,
 		C.idx_t(n),
 		(*C.float)(&x[0]),
 		C.idx_t(k),
@@ -220,16 +273,16 @@ func indexSearch(idx *C.FaissIndex, x []float32, k int64) (
 	return
 }
 
-func indexRangeSearch(idx *C.FaissIndex, x []float32, radius float32) (
+func (idx *faissIndex) RangeSearch(x []float32, radius float32) (
 	*RangeSearchResult, error,
 ) {
-	n := len(x) / indexD(idx)
+	n := len(x) / idx.D()
 	var rsr *C.FaissRangeSearchResult
 	if c := C.faiss_RangeSearchResult_new(&rsr, C.idx_t(n)); c != 0 {
 		return nil, getLastError()
 	}
 	if c := C.faiss_Index_range_search(
-		idx,
+		idx.idx,
 		C.idx_t(n),
 		(*C.float)(&x[0]),
 		C.float(radius),
@@ -240,103 +293,38 @@ func indexRangeSearch(idx *C.FaissIndex, x []float32, radius float32) (
 	return &RangeSearchResult{rsr}, nil
 }
 
-func indexReset(idx *C.FaissIndex) error {
-	if c := C.faiss_Index_reset(idx); c != 0 {
+func (idx *faissIndex) Reset() error {
+	if c := C.faiss_Index_reset(idx.idx); c != 0 {
 		return getLastError()
 	}
 	return nil
 }
 
-func indexRemoveIDs(idx *C.FaissIndex, sel *C.FaissIDSelector) (int, error) {
+func (idx *faissIndex) RemoveIDs(sel *IDSelector) (int, error) {
 	var nRemoved C.size_t
-	if c := C.faiss_Index_remove_ids(idx, sel, &nRemoved); c != 0 {
+	if c := C.faiss_Index_remove_ids(idx.idx, sel.sel, &nRemoved); c != 0 {
 		return 0, getLastError()
 	}
 	return int(nRemoved), nil
 }
 
-func indexDelete(idx *C.FaissIndex) {
-	C.faiss_Index_free(idx)
+func (idx *faissIndex) Delete() {
+	C.faiss_Index_free(idx.idx)
+}
+
+// IndexImpl is an abstract structure for an index.
+type IndexImpl struct {
+	Index
 }
 
 // AsFlat casts idx to a flat index.
 // AsFlat panics if idx is not a flat index.
-func (idx *Index) AsFlat() *IndexFlat {
-	ptr := C.faiss_IndexFlat_cast(idx.idx)
+func (idx *IndexImpl) AsFlat() *IndexFlat {
+	ptr := C.faiss_IndexFlat_cast(idx.cPtr())
 	if ptr == nil {
 		panic("index is not a flat index")
 	}
-	return &IndexFlat{ptr}
-}
-
-// D returns the dimension of the indexed vectors.
-func (idx *Index) D() int {
-	return indexD(idx.idx)
-}
-
-// IsTrained returns true if the index has been trained or does not require
-// training.
-func (idx *Index) IsTrained() bool {
-	return indexIsTrained(idx.idx)
-}
-
-// Ntotal returns the number of indexed vectors.
-func (idx *Index) Ntotal() int64 {
-	return indexNtotal(idx.idx)
-}
-
-// MetricType returns the metric type of the index.
-func (idx *Index) MetricType() int {
-	return indexMetricType(idx.idx)
-}
-
-// Train trains the index on a representative set of vectors.
-func (idx *Index) Train(x []float32) error {
-	return indexTrain(idx.idx, x)
-}
-
-// Add adds vectors to the index.
-func (idx *Index) Add(x []float32) error {
-	return indexAdd(idx.idx, x)
-}
-
-// AddWithIDs is like Add, but stores xids instead of sequential IDs.
-func (idx *Index) AddWithIDs(x []float32, xids []int64) error {
-	return indexAddWithIDs(idx.idx, x, xids)
-}
-
-// Search queries the index with the vectors in x.
-// Returns the IDs of the k nearest neighbors for each query vector and the
-// corresponding distances.
-func (idx *Index) Search(x []float32, k int64) (
-	distances []float32, labels []int64, err error,
-) {
-	return indexSearch(idx.idx, x, k)
-}
-
-// RangeSearch queries the index with the vectors in x.
-// Returns all vectors with distance < radius.
-// Note that many indexes do not implement RangeSearch.
-func (idx *Index) RangeSearch(x []float32, radius float32) (
-	*RangeSearchResult, error,
-) {
-	return indexRangeSearch(idx.idx, x, radius)
-}
-
-// Reset removes all vectors from the index.
-func (idx *Index) Reset() error {
-	return indexReset(idx.idx)
-}
-
-// RemoveIDs removes the vectors specified by sel from the index.
-// Returns the number of elements removed and error.
-func (idx *Index) RemoveIDs(sel *IDSelector) (int, error) {
-	return indexRemoveIDs(idx.idx, sel.sel)
-}
-
-// Delete frees the memory used by the index.
-func (idx *Index) Delete() {
-	indexDelete(idx.idx)
+	return &IndexFlat{&faissIndex{ptr}}
 }
 
 //--------------------------------------------------
@@ -345,15 +333,15 @@ func (idx *Index) Delete() {
 
 // IndexFactory builds a composite index.
 // description is a comma-separated list of components.
-func IndexFactory(d int, description string, metric int) (*Index, error) {
+func IndexFactory(d int, description string, metric int) (*IndexImpl, error) {
 	cdesc := C.CString(description)
 	defer C.free(unsafe.Pointer(cdesc))
-	var index Index
-	c := C.faiss_index_factory(&index.idx, C.int(d), cdesc, C.FaissMetricType(metric))
+	var idx faissIndex
+	c := C.faiss_index_factory(&idx.idx, C.int(d), cdesc, C.FaissMetricType(metric))
 	if c != 0 {
 		return nil, getLastError()
 	}
-	return &index, nil
+	return &IndexImpl{&idx}, nil
 }
 
 //--------------------------------------------------
@@ -363,20 +351,20 @@ func IndexFactory(d int, description string, metric int) (*Index, error) {
 // IndexFlat is an index that stores the full vectors and performs exhaustive
 // search.
 type IndexFlat struct {
-	idx *C.FaissIndexFlat
+	Index
 }
 
 // NewIndexFlat creates a new flat index.
 func NewIndexFlat(d int, metric int) (*IndexFlat, error) {
-	var index IndexFlat
+	var idx faissIndex
 	if c := C.faiss_IndexFlat_new_with(
-		&index.idx,
+		&idx.idx,
 		C.idx_t(d),
 		C.FaissMetricType(metric),
 	); c != 0 {
 		return nil, getLastError()
 	}
-	return &index, nil
+	return &IndexFlat{&idx}, nil
 }
 
 // NewIndexFlatIP creates a new flat index with the inner product metric type.
@@ -394,58 +382,6 @@ func NewIndexFlatL2(d int) (*IndexFlat, error) {
 func (idx *IndexFlat) Xb() []float32 {
 	var size C.size_t
 	var ptr *C.float
-	C.faiss_IndexFlat_xb(idx.idx, &ptr, &size)
+	C.faiss_IndexFlat_xb(idx.cPtr(), &ptr, &size)
 	return (*[1 << 30]float32)(unsafe.Pointer(ptr))[:size:size]
-}
-
-func (idx *IndexFlat) D() int {
-	return indexD(idx.idx)
-}
-
-func (idx *IndexFlat) IsTrained() bool {
-	return indexIsTrained(idx.idx)
-}
-
-func (idx *IndexFlat) Ntotal() int64 {
-	return indexNtotal(idx.idx)
-}
-
-func (idx *IndexFlat) MetricType() int {
-	return indexMetricType(idx.idx)
-}
-
-func (idx *IndexFlat) Train(x []float32) error {
-	return indexTrain(idx.idx, x)
-}
-
-func (idx *IndexFlat) Add(x []float32) error {
-	return indexAdd(idx.idx, x)
-}
-
-func (idx *IndexFlat) AddWithIDs(x []float32, xids []int64) error {
-	return indexAddWithIDs(idx.idx, x, xids)
-}
-
-func (idx *IndexFlat) Search(x []float32, k int64) (
-	distances []float32, labels []int64, err error,
-) {
-	return indexSearch(idx.idx, x, k)
-}
-
-func (idx *IndexFlat) RangeSearch(x []float32, radius float32) (
-	*RangeSearchResult, error,
-) {
-	return indexRangeSearch(idx.idx, x, radius)
-}
-
-func (idx *IndexFlat) Reset() error {
-	return indexReset(idx.idx)
-}
-
-func (idx *IndexFlat) RemoveIDs(sel *IDSelector) (int, error) {
-	return indexRemoveIDs(idx.idx, sel.sel)
-}
-
-func (idx *IndexFlat) Delete() {
-	indexDelete(idx.idx)
 }
