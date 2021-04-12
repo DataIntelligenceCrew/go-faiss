04d1e46dfd94f8543f2f73a188673dbaaa9ed2e6
crumbjp <hiroaki.kubota@mail.crumb.jp>
2021-04-12 19:51:56 +0900

    Add ReadIndex, WriteIndex

---
+++ index_io.go
@@ -0,0 +1,32 @@
+package faiss
+
+/*
+#include <stdlib.h>
+#include <faiss/c_api/index_io_c.h>
+*/
+import "C"
+import "unsafe"
+
+func WriteIndex(idx Index, filename string) error {
+	cfname := C.CString(filename)
+	defer C.free(unsafe.Pointer(cfname))
+	if c := C.faiss_write_index_fname(idx.cPtr(), cfname); c != 0 {
+		return getLastError()
+	}
+	return nil
+}
+
+const (
+	IOFlagMmap     = C.FAISS_IO_FLAG_MMAP
+	IOFlagReadOnly = C.FAISS_IO_FLAG_READ_ONLY
+)
+
+func ReadIndex(filename string, ioflags int) (*IndexImpl, error) {
+	cfname := C.CString(filename)
+	defer C.free(unsafe.Pointer(cfname))
+	var idx faissIndex
+	if c := C.faiss_read_index_fname(cfname, C.int(ioflags), &idx.idx); c != 0 {
+		return nil, getLastError()
+	}
+	return &IndexImpl{&idx}, nil
+}
