09cd7493627420377b83eee7f030f7d920b6d183
orihab <orielh@anyvision.co>
2021-08-18 14:10:38 +0300

    index_idmap.go: return Index interface instead of IDMapWrapper
    IDMapWrapper isn't practical bc I would like to be able to transfer
    gpuIndex (or any other index for that matter) to gpu using the
    transferToGpu. So I dont want a specific structure which wraps an idmap
    index, rather just return an Index interface.

--- index_idmap.go
+++ index_idmap.go
@@ -8,19 +8,12 @@ import (
 	"errors"
 )
 
-type IndexIDMapWrapper struct {
-	Index
-	cpointer **C.FaissIndex
-}
-
-func NewIndexIDMap(index *IndexFlat) (*IndexIDMapWrapper, error) {
+func NewIndexIDMap(index Index) (Index, error) {
 	var indexMapPointer *C.FaissIndexIDMap
 	var pointerToIndexMapPointer **C.FaissIndexIDMap
 	pointerToIndexMapPointer = &indexMapPointer
-	wrapper := IndexIDMapWrapper{cpointer: &indexMapPointer}
 	if C.faiss_IndexIDMap_new(pointerToIndexMapPointer, index.cPtr()) != 0 {
 		return nil, errors.New("Error occurred while initializing IndexIDMapWrapper")
 	}
-	wrapper.Index = &faissIndex{idx: *wrapper.cpointer}
-	return &wrapper, nil
+	return &faissIndex{idx: indexMapPointer}, nil
 }
